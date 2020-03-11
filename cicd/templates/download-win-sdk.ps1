664abb75978d0c1e08ba48c74791108189bbc530
sunbaodong <sbd021@126.com>
2020-03-11 22:31:18 +0800

    Update download-win-sdk.ps1

--- cicd/templates/download-win-sdk.ps1
+++ cicd/templates/download-win-sdk.ps1
@@ -1,2 +1,2 @@
 param($SDKUrl)
-Invoke-WebRequest -Uri "$SDKUrl" -OutFile .\AgoraSDK.zip -TimeoutSec 10;
+Invoke-WebRequest -Uri "https://download.agora.io/sdk/release/Agora_Native_SDK_for_Windows(x86)_v2_9_3_FULL.zip" -OutFile .\AgoraSDK.zip -TimeoutSec 10;
